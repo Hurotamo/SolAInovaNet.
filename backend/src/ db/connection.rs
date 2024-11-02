34bfda0ca03666c7825481dc11a4f9408b11c942
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-02 20:28:33 +0800

    Update connection.rs

--- backend/src/ db/connection.rs
+++ backend/src/ db/connection.rs
@@ -1,1 +1,10 @@
+use sqlx::{Pool, Postgres};
+use std::env;
+
+pub async fn establish_connection() -> Result<Pool<Postgres>, sqlx::Error> {
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set in the environment variables");
+    Pool::<Postgres>::connect(&database_url).await
+}
+
 
