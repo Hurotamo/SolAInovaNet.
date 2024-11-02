488e88738dd5b1804802c38028c5a4ee6ea8f505
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2024-11-02 20:30:19 +0800

    Update users_db.rs

--- backend/src/ db/users_db.rs
+++ backend/src/ db/users_db.rs
@@ -1,1 +1,24 @@
+use sqlx::PgPool;
+
+pub async fn create_user(pool: &PgPool, username: &str, email: &str) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        "INSERT INTO users (username, email) VALUES ($1, $2)",
+        username, email
+    )
+    .execute(pool)
+    .await?;
+    Ok(())
+}
+
+pub async fn get_user_by_id(pool: &PgPool, user_id: i32) -> Result<(String, String), sqlx::Error> {
+    let record = sqlx::query!(
+        "SELECT username, email FROM users WHERE id = $1",
+        user_id
+    )
+    .fetch_one(pool)
+    .await?;
+
+    Ok((record.username, record.email))
+}
+
 
